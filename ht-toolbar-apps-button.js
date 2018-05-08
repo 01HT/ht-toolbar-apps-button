a42b37186824df9b3d35ebb432dfc95eb06425de
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2018-05-08 16:26:46 +0300

    Update ht-toolbar-apps-button.js

--- ht-toolbar-apps-button.js
+++ ht-toolbar-apps-button.js
@@ -1,11 +1,12 @@
 "use strict";
 import { LitElement, html } from "@polymer/lit-element";
+import { repeat } from "lit-html/lib/repeat.js";
 import "@polymer/iron-iconset-svg";
 import "@polymer/paper-icon-button";
-import "@polymer/paper-styles/default-theme.js";
+import "@polymer/iron-dropdown";
 
 class HTToolabarAppsButton extends LitElement {
-  _render() {
+  _render({ items }) {
     return html`
       <style>
         :host {
@@ -17,17 +18,151 @@ class HTToolabarAppsButton extends LitElement {
         paper-icon-button {
           color: var(--secondary-text-color);
         }
+
+        iron-dropdown {
+          box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+          0 1px 10px 0 rgba(0, 0, 0, 0.12),
+          0 2px 4px -1px rgba(0, 0, 0, 0.4);
+          width: 270px;
+          overflow: hidden;
+          background: #fff;
+        }
+
+        a {
+            text-decoration: none;
+            color: inherit;
+            outline: none;
+
+        }
+
+        nav a {
+          display: flex;
+          flex-direction:column;
+          margin: 6px 0;
+          padding: 0 32px;
+          position:relative;
+        }
+
+        .border {
+          border-left: 3px solid var(--accent-color);
+          height: 100%;
+          position:absolute;
+          left:-3px;
+          transform: translateX(0);
+          transition: 0.5s;
+        }
+
+        a:hover .border {
+          transform: translateX(3px);
+          transition: 0.5s;
+        }
+
+        img {
+          display: block;
+          width: 18px;
+          height: 18px;
+          margin-right: 4px;
+        }
+
+        nav {
+          padding: 10px 0;
+          width:250px;
+          display:flex;
+          flex-direction: column;
+          overflow: hidden;
+        }
+
+        #title {
+          display:flex;
+          align-items:center;
+          font-size: 14px;
+          margin: 2px 0;
+        }
+
+        #company-text {
+          font-weight: 400;
+        }
+
+        #app-text {
+
+        }
+
+        #sub-text {
+          font-size: 13px;
+          font-weight:400;
+          color: var(--secondary-text-color);
+          transition: 0.15s opacity cubic-bezier(0.39, 0.575, 0.565, 1);
+          opacity: 0.6;
+        }
+
+        a:hover #sub-text {
+          opacity:1;
+        }
+
+        #divider {
+          height:1px;
+          width:100%;
+          background: var(--divider-color);
+        }
+
+        #support {
+          padding: 16px 32px;
+          color: var(--secondary-text-color);
+          font-size: 13px;
+        }
+
+        #support a {
+          text-decoration: underline;
+          font-weight: 600;
+        }
       </style>
       <iron-iconset-svg size="24" name="ht-toolbar-apps-button-icons">
-    <svg>
-        <defs>
-            <g id="apps">
-                <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z"></path>
-            </g>
-        </defs>
-    </svg>
-</iron-iconset-svg>
-<paper-icon-button icon="ht-toolbar-apps-button-icons:apps" on-click=${e => {this.toggle(e)}}></paper-icon-button>
+          <svg>
+              <defs>
+                  <g id="apps">
+                      <path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z"></path>
+                  </g>
+              </defs>
+          </svg>
+      </iron-iconset-svg>
+      <div id="container">
+        <paper-icon-button icon="ht-toolbar-apps-button-icons:apps" on-click=${e => {
+          e.preventDefault();
+          this.open();
+        }}></paper-icon-button>
+        
+        <iron-dropdown horizontal-align="right" vertical-align="top" vertical-offset="40" on-click="close">
+          <div slot="dropdown-content">
+            <div>
+              <nav>
+               ${repeat(
+                 items,
+                 item => html`
+                <a href="${item.href}" target="_blank">
+                  <div class="border"></div>
+                  <div id="title">
+                    <img src="/images/01ht-logo.svg">
+                    <div id="company-text">01HT</div>
+                    <div id="app-text" style="color:${item.color};">${
+                   item.appText
+                 }</div>
+                  </div>
+                  <div id="sub-text">
+                    ${item.subText}
+                  </div>
+                </a>
+              `
+               )}
+              </nav>
+              <div id="divider"></div>
+              <div id="support">
+                Присоединяйтесь к чату нашего сообщества в <a href="https://slack.com" target="_blank">Slack</a> 
+              </div>
+            <div>
+          </div>
+        </iron-dropdown>
+      </div>
+
 `;
   }
 
@@ -35,13 +170,56 @@ class HTToolabarAppsButton extends LitElement {
     return "ht-toolbar-apps-button";
   }
 
-  toggle(e) {
-    e.preventDefault();
-    this.dispatchEvent(
-      new CustomEvent("toggle", {
-        bubbles: false
-      })
-    );
+  static get properties() {
+    return {
+      items: Array
+    };
+  }
+
+  constructor() {
+    super();
+    this.items = [
+      {
+        href: "/",
+        appText: "Elements",
+        color: "#8bc34a",
+        subText: "Элементы обучения"
+      },
+      {
+        href: "/",
+        appText: "Console",
+        color: "#8bc34a",
+        subText: "Консоль управления LMS"
+      },
+      {
+        href: "/",
+        appText: "Account",
+        color: "#8bc34a",
+        subText: "Управление аккаунтом"
+      },
+      {
+        href: "/",
+        appText: "",
+        color: "#8bc34a",
+        subText: "О компании"
+      }
+    ];
+  }
+
+  get dropdown() {
+    return this.shadowRoot.querySelector("iron-dropdown");
+  }
+
+  open() {
+    if (this.dropdown.style.display === "") {
+      this.dropdown.close();
+    } else {
+      this.dropdown.open();
+    }
+  }
+
+  close() {
+    this.dropdown.close();
   }
 }
 
